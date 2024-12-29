f8e4fd464910e06ff4b05a13041b4aeeef9ec4e2
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-12-29 07:56:12 +0300

    "added Custom Snack Bar Widget "

--- lib/core/utils/core_utils.dart
+++ lib/core/utils/core_utils.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:hero_market/core/extensions/context_extensions.dart';
+import 'package:hero_market/core/extensions/text_style_extensions.dart';
+import 'package:hero_market/core/resources/styles/text.dart';
+
+import '../resources/styles/colors.dart';
 
 abstract class CoreUtils {
   const CoreUtils();
+  static void showSnackBar(
+    BuildContext context, {
+    required String message,
+    Color? backgroundColor,
+  }) {
+    final snackBar = SnackBar(
+      backgroundColor: backgroundColor ?? AppColors.lightThemePrimaryColor,
+      content: Center(
+        child: Text(
+          message,
+          style: AppTextStyles.paragraphSubTextRegular1.white,
+        ),
+      ),
+    );
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
 
   static Color adaptiveColor(
     BuildContext context, {
