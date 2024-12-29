b1d646a8547e29907b95fde0549373d421c4a220
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-12-29 08:17:41 +0300

    " Added Method To obscure Email "

--- lib/core/extensions/string_extensions.dart
+++ lib/core/extensions/string_extensions.dart
@@ -15,4 +15,25 @@ extension StringExt on String {
       _ => ThemeMode.system,
     };
   }
+
+  String get obscureEmail {
+    // Ensure the email contains an '@' character
+    final index = indexOf('@');
+    if (index == -1) {
+      throw FormatException('Invalid email format');
+    }
+
+    // Split the email into username and domain
+    var username = substring(0, index);
+    final domain = substring(index + 1);
+
+    // Handle short usernames
+    if (username.length <= 2) {
+      return '$username@$domain';
+    }
+
+    // Obscure the username
+    username = '${username[0]}****${username[username.length - 1]}';
+    return '$username@$domain';
+  }
 }
