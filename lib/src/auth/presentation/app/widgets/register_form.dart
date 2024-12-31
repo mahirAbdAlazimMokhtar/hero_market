f60aeb33ce86ffc6654d5805ba4e2daac2643005
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-12-31 17:27:06 +0300

    Refactor Login and Register screens to enhance user experience and streamline navigation

--- lib/src/auth/presentation/app/widgets/register_form.dart
+++ lib/src/auth/presentation/app/widgets/register_form.dart
@@ -1,5 +1,6 @@
 import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
@@ -9,7 +10,8 @@ import 'package:hero_market/core/common/widgets/vertical_label_field.dart';
 import 'package:hero_market/core/extensions/text_style_extensions.dart';
 import 'package:hero_market/core/extensions/widgets_extensions.dart';
 import 'package:hero_market/core/resources/styles/text.dart';
-import 'package:hero_market/src/auth/domain_layer/usecases/forget_password.dart';
+import 'package:hero_market/core/utils/core_utils.dart';
+import 'package:hero_market/src/auth/presentation/app/adapter/cubit/auth_cubit.dart';
 
 import '../../../../../core/resources/styles/colors.dart';
 import '../screens/forgot_password_screen.dart';
@@ -70,157 +72,193 @@ class _RegistrationFormState extends State<RegistrationForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          VerticalLabelField(
-            label: 'Full Name',
-            controller: fullNameController,
-            hintText: 'Enter your full name',
-            keyboardType: TextInputType.name,
-          ),
-          Gap(20),
-           VerticalLabelField(
-            label: 'Email',
-            controller: emailController,
-            hintText: 'Enter your Email',
-            keyboardType: TextInputType.emailAddress,
-          ),
-          Gap(30),
-          ValueListenableBuilder(
-            valueListenable: countryNotifier,
-            builder: (context, country, __) {
-              return Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // حقل رمز الدولة
-                  Flexible(
-                    flex: 1,
-                    child: InputField(
-                      controller: countryController,
-                      defaultValidation: false,
-                      readOnly: true,
-                      contentPadding: const EdgeInsets.only(left: 10),
-                      suffixIcon: GestureDetector(
-                        onTap: pickCountry,
-                        child: const Icon(Icons.arrow_drop_down),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state case AuthError(:final message)) {
+          CoreUtils.showSnackBar(context, message: message);
+        } else if (state is Registered) {
+          context.go('/');
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: formKey,
+          child: Column(
+            children: [
+              VerticalLabelField(
+                label: 'Full Name',
+                controller: fullNameController,
+                hintText: 'Enter your full name',
+                keyboardType: TextInputType.name,
+              ),
+              Gap(20),
+              VerticalLabelField(
+                label: 'Email',
+                controller: emailController,
+                hintText: 'Enter your Email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              Gap(30),
+              ValueListenableBuilder(
+                valueListenable: countryNotifier,
+                builder: (context, country, __) {
+                  return Column(
+                    spacing: 10,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Phone Number',
+                          style: AppTextStyles.paragraphSubTextRegular1
+                              .adaptiveColor(context)),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // حقل رمز الدولة
+                          Flexible(
+                            flex: 1,
+                            child: InputField(
+                              controller: countryController,
+                              defaultValidation: false,
+                              readOnly: true,
+                              contentPadding: const EdgeInsets.only(left: 10),
+                              suffixIcon: GestureDetector(
+                                onTap: pickCountry,
+                                child: const Icon(Icons.arrow_drop_down),
+                              ),
+                              suffixIconConstraints: const BoxConstraints(),
+                              validator: (value) {
+                                if (!isPhoneValid(
+                                  phoneNumberController.text,
+                                  defaultCountryCode: country?.countryCode,
+                                )) {
+                                  return '';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          const SizedBox(width: 10), // مسافة بين الحقول
+                      
+                          Flexible(
+                            flex: 3,
+                            child: InputField(
+                              controller: phoneNumberController,
+                              keyboardType: TextInputType.phone,
+                              hintText: 'Enter your phone number',
+                              enabled: country != null,
+                              validator: (value) {
+                                if (!isPhoneValid(
+                                  value ?? '',
+                                  defaultCountryCode: country?.countryCode ?? 'US',
+                                )) {
+                                  return 'Invalid Phone number';
+                                }
+                                return null;
+                              },
+                              inputFormatters: [
+                                PhoneInputFormatter(
+                                  defaultCountryCode: country?.countryCode ?? 'US',
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                      suffixIconConstraints: const BoxConstraints(),
-                      validator: (value) {
-                        if (!isPhoneValid(
-                          phoneNumberController.text,
-                          defaultCountryCode: country?.countryCode ,
-                        )) {
-                          return '';
-                        }
-                        return null;
-                      },
+                    ],
+                  );
+                },
+              ),
+              const Gap(20),
+              ValueListenableBuilder(
+                valueListenable: obscurePasswordNotifier,
+                builder: (_, obscurePassword, __) {
+                  return VerticalLabelField(
+                    label: 'Password',
+                    controller: passwordController,
+                    hintText: 'Enter your password',
+                    obscureText: obscurePassword,
+                    suffixIcon: GestureDetector(
+                      onTap: () =>
+                          obscurePasswordNotifier.value = !obscurePassword,
+                      child: Icon(
+                        obscurePassword
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: AppColors.lightThemeSecondaryTextColor,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const Gap(20),
+              ValueListenableBuilder(
+                valueListenable: obscureConfirmPasswordNotifier,
+                builder: (_, obscureConfirmPassword, __) {
+                  return VerticalLabelField(
+                    label: 'Confirm Password',
+                    controller: confirmPasswordController,
+                    hintText: 'please confirm your password',
+                    obscureText: obscureConfirmPassword,
+                    suffixIcon: GestureDetector(
+                      onTap: () => obscureConfirmPasswordNotifier.value =
+                          !obscureConfirmPasswordNotifier.value,
+                      child: Icon(
+                        obscureConfirmPassword
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: AppColors.lightThemeSecondaryTextColor,
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value != passwordController.text.trim()) {
+                        return 'Password does not match';
+                      }
+                      return null;
+                    },
+                  );
+                },
+              ),
+              const Gap(20),
+              SizedBox(
+                width: double.maxFinite,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      context.go(ForgotPasswordScreen.path);
+                    },
+                    child: Text(
+                      'Forgot Password?',
+                      style: AppTextStyles.paragraphSubTextRegular1
+                          .adaptiveColor(context),
                     ),
                   ),
-                  const SizedBox(width: 10), // مسافة بين الحقول
-
-                  Flexible(
-                    flex: 3,
-                    child: InputField(
-                      controller: phoneNumberController,
-                      keyboardType: TextInputType.phone,
-                      hintText: 'Enter your phone number',
-                      enabled: country != null,
-                      validator: (value) {
-                        if (!isPhoneValid(
-                          value ?? '',
-                          defaultCountryCode: country?.countryCode ?? 'US',
-                        )) {
-                          return 'Invalid Phone number';
-                        }
-                        return null;
-                      },
-                      inputFormatters: [
-                        PhoneInputFormatter(
-                          defaultCountryCode: country?.countryCode ?? 'US',
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-          const Gap(20),
-          ValueListenableBuilder(
-            valueListenable: obscurePasswordNotifier,
-            builder: (_, obscurePassword, __) {
-              return VerticalLabelField(
-                label: 'Password',
-                controller: passwordController,
-                hintText: 'Enter your password',
-                obscureText: obscurePassword,
-                suffixIcon: GestureDetector(
-                  onTap: () => obscurePasswordNotifier.value = !obscurePassword,
-                  child: Icon(
-                    obscurePassword
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility_outlined,
-                    color: AppColors.lightThemeSecondaryTextColor,
-                  ),
-                ),
-              );
-            },
-          ),
-          const Gap(20),
-          ValueListenableBuilder(
-            valueListenable: obscureConfirmPasswordNotifier,
-            builder: (_, obscureConfirmPassword, __) {
-              return VerticalLabelField(
-                label: 'Confirm Password',
-                controller: confirmPasswordController,
-                hintText: 'please confirm your password',
-                obscureText: obscureConfirmPassword,
-                suffixIcon: GestureDetector(
-                  onTap: () => obscureConfirmPasswordNotifier.value =
-                      !obscureConfirmPasswordNotifier.value,
-                  child: Icon(
-                    obscureConfirmPassword
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility_outlined,
-                    color: AppColors.lightThemeSecondaryTextColor,
-                  ),
-                ),
-                validator: (value) {
-                  if (value != passwordController.text.trim()) {
-                    return 'Password does not match';
-                  }
-                  return null;
-                },
-              );
-            },
-          ),
-          const Gap(20),
-          SizedBox(
-            width: double.maxFinite,
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () {
-                  context.go(ForgotPasswordScreen.path);
-                },
-                child: Text(
-                  'Forgot Password?',
-                  style: AppTextStyles.paragraphSubTextRegular1
-                      .adaptiveColor(context),
                 ),
               ),
-            ),
+              const Gap(40),
+              RoundedButton(
+                  text: 'Sign Up',
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      final phoneNumber = phoneNumberController.text.trim();
+                      final country = countryNotifier.value!;
+                      final formattedNumber =
+                          '+${country.phoneCode}${toNumericString(phoneNumber)}';
+                      final fullName = fullNameController.text.trim();
+                      final email = emailController.text.trim();
+                      final password = passwordController.text.trim();
+                      context.read<AuthCubit>().register(
+                            fullName: fullName,
+                            email: email,
+                            password: password,
+                            phoneNumber:
+                                formattedNumber, // هنا يتم إرسال الرقم بعد تنسيقه
+                          );
+                    }
+                  }).loading(state is AuthLoading),
+            ],
           ),
-          const Gap(40),
-          RoundedButton(
-              text: 'Sign Up',
-              onPressed: () {
-                if (formKey.currentState!.validate()) {}
-              }).loading(false),
-        ],
-      ),
+        );
+      },
     );
   }
 }
