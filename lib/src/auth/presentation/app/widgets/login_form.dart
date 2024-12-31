5bad0f6178ed9f991a79b5febe51df46ebd86cf4
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2024-12-31 17:25:43 +0300

    Implement BlocConsumer in LoginForm for improved state management and error handling

--- lib/src/auth/presentation/app/widgets/login_form.dart
+++ lib/src/auth/presentation/app/widgets/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hero_market/core/common/widgets/rounded_button.dart';
@@ -6,6 +7,8 @@ import 'package:hero_market/core/common/widgets/vertical_label_field.dart';
 import 'package:hero_market/core/extensions/text_style_extensions.dart';
 import 'package:hero_market/core/extensions/widgets_extensions.dart';
 import 'package:hero_market/core/resources/styles/text.dart';
+import 'package:hero_market/core/utils/core_utils.dart';
+import 'package:hero_market/src/auth/presentation/app/adapter/cubit/auth_cubit.dart';
 
 import '../../../../../core/resources/styles/colors.dart';
 import '../screens/forgot_password_screen.dart';
@@ -33,61 +36,78 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          VerticalLabelField(
-            label: 'Email',
-            controller: emailController,
-            hintText: 'Enter your email',
-            keyboardType: TextInputType.emailAddress,
-          ),
-          const Gap(20),
-          ValueListenableBuilder(
-            valueListenable: obscurePasswordNotifier,
-            builder: (_, obscurePassword, __) {
-              return VerticalLabelField(
-                label: 'Password',
-                controller: passwordController,
-                hintText: 'Enter your password',
-                obscureText: obscurePassword,
-                suffixIcon: GestureDetector(
-                  onTap: () => obscurePasswordNotifier.value = !obscurePassword,
-                  child: Icon(
-                    obscurePassword
-                        ? Icons.visibility_off_outlined
-                        : Icons.visibility_outlined,
-                    color: AppColors.lightThemeSecondaryTextColor,
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state case AuthError(:final message)) {
+          CoreUtils.showSnackBar(context,message: message);
+        }else if (state is LoggedIn){
+          context.go('/',extra: 'home');
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: formKey,
+          child: Column(
+            children: [
+              VerticalLabelField(
+                label: 'Email',
+                controller: emailController,
+                hintText: 'Enter your email',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const Gap(20),
+              ValueListenableBuilder(
+                valueListenable: obscurePasswordNotifier,
+                builder: (_, obscurePassword, __) {
+                  return VerticalLabelField(
+                    label: 'Password',
+                    controller: passwordController,
+                    hintText: 'Enter your password',
+                    obscureText: obscurePassword,
+                    suffixIcon: GestureDetector(
+                      onTap: () =>
+                          obscurePasswordNotifier.value = !obscurePassword,
+                      child: Icon(
+                        obscurePassword
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                        color: AppColors.lightThemeSecondaryTextColor,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              const Gap(20),
+              SizedBox(
+                width: double.maxFinite,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      context.go(ForgotPasswordScreen.path);
+                    },
+                    child: Text(
+                      'Forgot Password?',
+                      style: AppTextStyles.paragraphSubTextRegular1
+                          .adaptiveColor(context),
+                    ),
                   ),
                 ),
-              );
-            },
-          ),
-          const Gap(20),
-          SizedBox(
-            width: double.maxFinite,
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: GestureDetector(
-                onTap: () {
-                  context.go(ForgotPasswordScreen.path);
-                },
-                child: Text(
-                  'Forgot Password?',
-                  style: AppTextStyles.paragraphSubTextRegular1
-                      .adaptiveColor(context),
-                ),
               ),
-            ),
+              const Gap(40),
+              RoundedButton(
+                  text: 'Sign In',
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      final email = emailController.text.trim();
+                      final password = passwordController.text.trim();
+                      context.read<AuthCubit>().login(email, password);
+                    }
+                  }).loading(state is AuthLoading),
+            ],
           ),
-          const Gap(40),
-          RoundedButton(text: 'Sign In', onPressed: () {
-            if (formKey.currentState!.validate()) {}
-            
-          }).loading(false),
-        ],
-      ),
+        );
+      },
     );
   }
 }
