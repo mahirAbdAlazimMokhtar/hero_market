ed5e3ace74122bd7877fed7ad06a2eb9de08eb6e
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2025-01-04 20:39:17 +0300

    Removed file from tracking and updated .gitignore

--- lib/core/utils/constants/network_constants.dart
+++
@@ -1,7 +0,0 @@
-abstract class NetworkConstants {
-  static const String baseUrl = 'http://127.0.0.1:3000/api/v1';
-  static const authority = '127.0.0.1:3000';
-  static const apiUrl = 'api/v1';
-  static const headers = {'Content-Type': 'application/json; charset=UTF-8'};
-  static const pageSize = 10;
-}
