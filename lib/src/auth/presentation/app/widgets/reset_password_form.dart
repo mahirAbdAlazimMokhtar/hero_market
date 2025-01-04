05a93515f273334d22146c07cef7abc1669d83f9
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2025-01-04 11:25:47 +0300

    Integrate Bloc for password reset form to enhance state management and user feedback

--- lib/src/auth/presentation/app/widgets/reset_password_form.dart
+++ lib/src/auth/presentation/app/widgets/reset_password_form.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hero_market/core/extensions/widgets_extensions.dart';
+import 'package:hero_market/src/auth/presentation/app/adapter/cubit/auth_cubit.dart';
 
 import '../../../../../core/common/widgets/rounded_button.dart';
 import '../../../../../core/common/widgets/vertical_label_field.dart';
+import '../../../../../core/utils/core_utils.dart';
 
 class ResetPasswordForm extends StatefulWidget {
   const ResetPasswordForm({required this.email, super.key});
@@ -32,76 +36,89 @@ class _ResetPasswordFormState extends State<ResetPasswordForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        children: [
-          ValueListenableBuilder(
-            valueListenable: obscurePasswordNotifier,
-            builder: (_, obscurePassword, __) {
-              return VerticalLabelField(
-                label: 'Password',
-                hintText: 'Enter your password',
-                controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: obscurePassword,
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    obscurePasswordNotifier.value =
-                        !obscurePasswordNotifier.value;
-                  },
-                  child: Icon(
-                    switch (obscurePassword) {
-                      true => Icons.visibility_off_outlined,
-                      _ => Icons.visibility_outlined,
-                    },
-                  ),
-                ),
-              );
-            },
-          ),
-          const Gap(20),
-          ValueListenableBuilder(
-            valueListenable: obscureConfirmPasswordNotifier,
-            builder: (_, obscureConfirmPassword, __) {
-              return VerticalLabelField(
-                label: 'Confirm Password',
-                hintText: 'Re-enter your password',
-                controller: confirmPasswordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: obscureConfirmPassword,
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    obscureConfirmPasswordNotifier.value =
-                        !obscureConfirmPasswordNotifier.value;
-                  },
-                  child: Icon(
-                    switch (obscureConfirmPassword) {
-                      true => Icons.visibility_off_outlined,
-                      _ => Icons.visibility_outlined,
-                    },
-                  ),
-                ),
-                validator: (value) {
-                  if (value! != passwordController.text.trim()) {
-                    return 'Passwords do not match';
-                  }
-                  return null;
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state case AuthError(:final message)) {
+          CoreUtils.showSnackBar(context, message: message);
+        } else if (state is PasswordReset) {
+          CoreUtils.showSnackBar(context, message: 'Password reset successful!');
+          context.go('/');
+        }
+      },
+      builder: (context, state) {
+        return Form(
+          key: formKey,
+          child: Column(
+            children: [
+              ValueListenableBuilder(
+                valueListenable: obscurePasswordNotifier,
+                builder: (_, obscurePassword, __) {
+                  return VerticalLabelField(
+                    label: 'Password',
+                    hintText: 'Enter your password',
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: obscurePassword,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        obscurePasswordNotifier.value =
+                            !obscurePasswordNotifier.value;
+                      },
+                      child: Icon(
+                        switch (obscurePassword) {
+                          true => Icons.visibility_off_outlined,
+                          _ => Icons.visibility_outlined,
+                        },
+                      ),
+                    ),
+                  );
                 },
-              );
-            },
+              ),
+              const Gap(20),
+              ValueListenableBuilder(
+                valueListenable: obscureConfirmPasswordNotifier,
+                builder: (_, obscureConfirmPassword, __) {
+                  return VerticalLabelField(
+                    label: 'Confirm Password',
+                    hintText: 'Re-enter your password',
+                    controller: confirmPasswordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: obscureConfirmPassword,
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        obscureConfirmPasswordNotifier.value =
+                            !obscureConfirmPasswordNotifier.value;
+                      },
+                      child: Icon(
+                        switch (obscureConfirmPassword) {
+                          true => Icons.visibility_off_outlined,
+                          _ => Icons.visibility_outlined,
+                        },
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value! != passwordController.text.trim()) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                  );
+                },
+              ),
+              const Gap(40),
+              RoundedButton(
+                  text: 'Submit',
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      context.read<AuthCubit>().resetPassword(
+                          email: widget.email,
+                          newPassword: passwordController.text.trim());
+                    }
+                  }).loading(state is AuthLoading),
+            ],
           ),
-          const Gap(40),
-          RoundedButton(
-            text: 'Submit',
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                // TODO(Reset-Password): Implement resetPassword functionality.
-              }
-            },
-          ).loading(false),
-        ],
-      ),
+        );
+      },
     );
   }
 }
