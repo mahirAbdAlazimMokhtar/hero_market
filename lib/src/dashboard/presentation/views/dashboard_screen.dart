6b1a0dd1026295109db2bf1bc5eb5eec5f47b6e6
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2025-01-29 18:41:28 +0300

    Add DashboardDrawer and enhance DashboardScreen with navigation and drawer functionality

--- lib/src/dashboard/presentation/views/dashboard_screen.dart
+++ lib/src/dashboard/presentation/views/dashboard_screen.dart
@@ -1,6 +1,19 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hero_market/src/on_boarding/presentation/utilities/dashboard_utils.dart';
+import 'package:hero_market/core/extensions/context_extensions.dart';
+import 'package:hero_market/core/resources/styles/colors.dart';
+import 'package:hero_market/core/utils/core_utils.dart';
+import 'package:hero_market/src/dashboard/presentation/app/dashboard_state.dart';
+import 'package:hero_market/src/dashboard/presentation/views/widgets/dashboard_drawer.dart';
+import 'package:hero_market/src/explore/presentation/views/explore_view.dart';
+import 'package:hero_market/src/home/presentation/views/home_views.dart';
+
+import '../../../cart/presentation/views/cart_view.dart';
+import '../../../user/presentation/views/profile_view.dart';
+import '../../../wishlist/presentation/views/wishlist_view.dart';
+import '../utils/dashboard_utils.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key, required this.state, required this.child});
@@ -9,9 +22,53 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final activeIndex = DashboardUtils.activeIndex(state);
     return Scaffold(
-      key:  DashboardUtils.scaffoldKey,
+      key: DashboardUtils.scaffoldKey,
       body: child,
+      drawer: DashboardDrawer(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: DashboardState.instance.indexNotifier,
+        builder: (_,currentIndex,__) {
+          return CurvedNavigationBar(
+            index: currentIndex,
+            backgroundColor: context.theme.scaffoldBackgroundColor,
+            color: CoreUtils.adaptiveColor(context,
+                lightModeColor: AppColors.lightThemeWhiteColor,
+                darkModeColor: AppColors.darkThemeDarkSharpColor),
+            buttonBackgroundColor: AppColors.lightThemePrimaryColor,
+            items: DashboardUtils.iconList.mapIndexed((index, icon) {
+              final isActive = activeIndex == index;
+              return Icon(
+                isActive ? icon.active : icon.idle,
+                size: 30,
+                color: isActive
+                    ? AppColors.lightThemeWhiteColor
+                    : AppColors.lightThemePrimaryColor,
+              );
+            }).toList(),
+            onTap: (index)async {
+              final currentIndex = activeIndex;
+              DashboardState.instance.changeIndex(index);
+              switch (index) {
+                case 0:
+                  context.go(HomeViews.path);
+                case 1:
+                  context.go(ExploreView.path);
+                case 2:
+                  await context.push(CartView.path);
+                  DashboardState.instance.changeIndex(currentIndex);
+                case 3:
+                  context.go(WishlistView.path);
+                case 4:
+                  await context.push(ProfileView.path);
+                  DashboardState.instance.changeIndex(currentIndex);
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
---
+++ lib/src/dashboard/presentation/views/widgets/dashboard_drawer.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hero_market/core/common/widgets/rounded_button.dart';
+import 'package:hero_market/core/extensions/string_extensions.dart';
+import 'package:hero_market/core/extensions/text_style_extensions.dart';
+import 'package:hero_market/core/extensions/widgets_extensions.dart';
+import 'package:hero_market/core/resources/styles/colors.dart';
+import 'package:hero_market/core/resources/styles/text.dart';
+import 'package:hero_market/core/utils/core_utils.dart';
+import 'package:hero_market/src/dashboard/presentation/app/dashboard_state.dart';
+import 'package:hero_market/src/dashboard/presentation/utils/dashboard_utils.dart';
+import 'package:hero_market/src/user/presentation/views/profile_view.dart';
+import 'package:hero_market/src/wishlist/presentation/views/wishlist_view.dart';
+
+import '../../../../../core/common/app/cache_helper.dart';
+import '../../../../../core/common/widgets/bottom_sheet_card.dart';
+import '../../../../../core/services/injection_container.dart';
+
+class DashboardDrawer extends StatefulWidget {
+  const DashboardDrawer({super.key});
+
+  @override
+  State<DashboardDrawer> createState() => _DashboardDrawerState();
+}
+
+class _DashboardDrawerState extends State<DashboardDrawer> {
+  final signingOutNotifier = ValueNotifier(false);
+  @override
+  void dispose() {
+    signingOutNotifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: CoreUtils.adaptiveColor(context,
+          lightModeColor: AppColors.lightThemeWhiteColor,
+          darkModeColor: AppColors.darkThemeDarkSharpColor),
+      child: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundColor: AppColors.lightThemePrimaryColor,
+                    child: Center(
+                      child: Text(
+                        'Mahir Mokhtar'.initials,
+                        style: AppTextStyles.headingMedium.white,
+                      ),
+                    ),
+                  ),
+                  Gap((15)),
+                  Text(
+                    'Mahir Mokhtar',
+                    style: AppTextStyles.headingMedium.adaptiveColor(context),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: ListView.separated(
+              separatorBuilder: (_, __) => Divider(
+                color: CoreUtils.adaptiveColor(context,
+                    lightModeColor: AppColors.lightThemeWhiteColor,
+                    darkModeColor: AppColors.darkThemeDarkNavBarColor),
+              ),
+              itemCount: DashboardUtils.drawerItems.length,
+              itemBuilder: (_, index) {
+                final drawerItem = DashboardUtils.drawerItems[index];
+                return ListTile(
+                    leading: Icon(
+                      drawerItem.icon,
+                      color: AppColors.classicAdaptiveTextColor(context),
+                    ),
+                    title: Text(
+                      drawerItem.title,
+                      style:
+                          AppTextStyles.headingMedium3.adaptiveColor(context),
+                    ),
+                    onTap: () {
+                      if (index != 1) Scaffold.of(context).closeDrawer();
+                      switch (index) {
+                        case 0:
+                          context.push(ProfileView.path);
+                        case 1:
+                        //TODO: implement payment profile
+                        case 2:
+                          DashboardState.instance.changeIndex(3);
+                          context.go(WishlistView.path);
+                        case 3:
+                        //: implement my orders
+                      }
+                    });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(
+              bottom: 50,
+              top: 10,
+            ),
+            child: ValueListenableBuilder(
+              valueListenable: signingOutNotifier,
+              builder: (_, value, __) {
+                return RoundedButton(
+                  text: 'Sign Out',
+                  height: 50,
+                  onPressed: () async {
+                    final router = GoRouter.of(context);
+                    final result = await showModalBottomSheet<bool>(
+                      context: context,
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                      isDismissible: false,
+                      builder: (_) {
+                        return BottomSheetCard(
+                          title: 'Are you sure you want to sign out?',
+                          positiveButtonText: 'Yes',
+                          negativeButtonText: 'Cancel',
+                          positiveButtonColor: AppColors.lightThemeSecondaryColor,
+                        );
+                      },
+                    );
+                    if (result ?? false) {
+                      signingOutNotifier.value = true;
+                      await sl<CacheHelper>().resetSession();
+                      router.go('/');
+                    }
+                  },
+                ).loading(value);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
