d32f87fcffd1ca72b7e94f0ec4399fab1d61e54a
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2025-02-13 19:08:19 +0300

    Add product-related dependencies and initialize ProductCubit in the injection container

--- lib/core/services/injection_container.main.dart
+++ lib/core/services/injection_container.main.dart
@@ -6,6 +6,42 @@ Future<void> init() async {
   await _cacheInit();
   await _authInit();
   await _userInit();
+  await _productInit();
+}
+
+Future<void> _productInit() async {
+  sl
+    ..registerFactory(
+      () => ProductCubit(
+        getCategories: sl(),
+        getCategory: sl(),
+        getPopular: sl(),
+        getProductReviews: sl(),
+        getProduct: sl(),
+        getProductsByCategory: sl(),
+        getProducts: sl(),
+        leaveReview: sl(),
+        searchAllProducts: sl(),
+        searchByCategory: sl(),
+        searchByCategoryAndGenderAgeCategory: sl(),
+        getNewArrivals: sl(),
+      ),
+    )
+    ..registerLazySingleton(() => GetCategories(sl()))
+    ..registerLazySingleton(() => GetCategory(sl()))
+    ..registerLazySingleton(() => GetPopular(sl()))
+    ..registerLazySingleton(() => GetProductReviews(sl()))
+    ..registerLazySingleton(() => GetProduct(sl()))
+    ..registerLazySingleton(() => GetProductsByCategory(sl()))
+    ..registerLazySingleton(() => GetProducts(sl()))
+    ..registerLazySingleton(() => LeaveReview(sl()))
+    ..registerLazySingleton(() => SearchAllProducts(sl()))
+    ..registerLazySingleton(() => SearchByCategory(sl()))
+    ..registerLazySingleton(() => SearchByCategoryAndGenderAgeCategory(sl()))
+    ..registerLazySingleton(() => GetNewArrivals(sl()))
+    ..registerLazySingleton<ProductRepo>(() => ProductRepoImpl(sl()))
+    ..registerLazySingleton<ProductModelsRemoteDataSrc>(
+        () => ProductRemoteDataSrcImpl(sl()));
 }
 
 Future<void> _userInit() async {
@@ -50,12 +86,11 @@ Future<void> _authInit() async {
     ..registerLazySingleton(http.Client.new);
 }
 
-
 Future<void> _cacheInit() async {
   final prefs = await SharedPreferences.getInstance();
 
-  sl.registerSingleton<SharedPreferences>(prefs); // تسجيل SharedPreferences أولاً
-  sl.registerLazySingleton<CacheHelper>(() => CacheHelper(sl())..getThemeMode());
+  sl.registerSingleton<SharedPreferences>(
+      prefs); // تسجيل SharedPreferences أولاً
+  sl.registerLazySingleton<CacheHelper>(
+      () => CacheHelper(sl())..getThemeMode());
 }
-
-
