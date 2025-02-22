32fafb9521203b3ca72f59dababe6acd6614d11b
Mahir Abd Alazim <40815395+mahirAbdAlazimMokhtar@users.noreply.github.com>
2025-02-22 19:51:17 +0300

    Refactor HomeViews to use dedicated sections for app bar, search, categories, and products

--- lib/src/home/presentation/views/home_views.dart
+++ lib/src/home/presentation/views/home_views.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hero_market/core/common/app/providers/user_provider.dart';
-import 'package:hero_market/core/extensions/context_extensions.dart';
-import 'package:hero_market/core/extensions/text_style_extensions.dart';
+import 'package:gap/gap.dart';
+import 'package:hero_market/src/home/presentation/sections/home_app_bar.dart';
+import 'package:hero_market/src/home/presentation/sections/product_section.dart';
+import 'package:hero_market/src/home/presentation/widgets/promo_banner.dart';
+
+
+import '../sections/category_section.dart';
+import '../sections/search_section.dart';
 
 class HomeViews extends StatelessWidget {
-  const HomeViews({super.key});
+  HomeViews({super.key});
   static const path = '/home';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: Center(
-        child: Text(
-          context.watch<UserProvider>().currentUSer!.name,
-          style: context.theme.textTheme.bodyLarge?.adaptiveColor(context),
-        ),
-      ),
-    );
+        appBar: HomeAppBar(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const Gap(40),
+              SearchSection(
+                onTap: () {
+                  //TODO : push to search screen
+                },
+              ),
+              const Gap(20),
+              Expanded(
+                  child: ListView(
+                children: [
+                  //Promo Banner
+                  PromoBanner(),
+                  const Gap(20),
+                  CategorySection(),
+                  const Gap(20),
+                  ProductSection.popular(
+                    onViewAll: () {
+                      //TODO : push to popular screen
+                    },
+                  ),
+                  const Gap(20),
+                  ProductSection.newArrivals(
+                    onViewAll: () {
+                      //TODO : push to newArrivals screen
+                    },
+                  ),
+                  //New Arrivals Section
+                ],
+              ))
+            ],
+          ),
+        ));
   }
 }
